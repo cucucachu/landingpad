82fd8f4cb5b8342cc21fc1d17b41e3cbe886feaf
Cody Jones <Cody Jones>
2017-03-04 20:30:07 -0800

    Test Video

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('upload', { title: 'Express' });
+});
+
+router.get('/testvideo', function(req, res, next) {
+	res.writeHead(200, {'Content-Type': 'video/mp4'});
+	var rs = fs.createReadStream('public/candle.mp4');
+	rs.pipe(res);
 });
 
 module.exports = router;
