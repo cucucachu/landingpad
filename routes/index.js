8fd324c8202fd2e59dc9bb0029c932b1504355a7
mcdito13 <mcdito13@gmail.com>
2017-03-05 17:10:56 -0800

    Upload Watch Find tabs created

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/upload', function(req, res, next) {
   res.render('upload', { title: 'Express' });
+});
+
+router.get('/watch', function(req, res, next) {
+  res.render('watch', { title: 'Express' });
+});
+
+router.get('/find', function(req, res, next) {
+  res.render('find', { title: 'Express' });
 });
 
 router.get('/testvideo', function(req, res, next) {
